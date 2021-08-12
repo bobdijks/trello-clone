c5905456848bfcfadbd01c334b8dfe2aa8a45687
Bob Dijks <rhcdijks@gmail.com>
2021-08-11 21:50:37 -0500

    finished logic for creating new cards and saving to json

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,19 +1,55 @@
 <template>
-  <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
-      <h6>{{ list.name }}</h6>
-      <hr />
+  <div class="container">
+    <div id="app" class="row">
+      <div v-for="(list, index) in original_lists" class="col-3">
+        <h6>{{ list.name }}</h6>
 
-      <div v-for="(card, index) in list.cards" class="card card-body">
-        {{ card.name }}
+        <hr />
+
+        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+          {{ card.name }}
+        </div>
+
+        <div class="card card-body">
+          <textarea v-model="messages[list.id]" class="form-control"></textarea>
+          <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from '@rails/ujs';
+
 export default {
  props: ["original_lists"],
+ data: function() {
+   return {
+     messages: {},
+     lists: this.original_lists,
+   }
+ },
+  methods: {
+    submitMessages: function(list_id) {
+      var data = new FormData
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.messages[list_id])
+
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id)
+          this.list[index].cards.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
+    }
+  }
 }
 </script>
 
